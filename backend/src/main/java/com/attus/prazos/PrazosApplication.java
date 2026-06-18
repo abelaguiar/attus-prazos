a0370bd988086cebe3b32239f350b70ea8336825
Abel Aguiar <abel.prog@gmail.com>
2026-06-18 12:48:48 -0300

    init: inicializa estrutura base da API Java Spring Boot

---
+++ backend/src/main/java/com/attus/prazos/PrazosApplication.java
@@ -0,0 +1,13 @@
+package com.attus.prazos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrazosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrazosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/attus/prazos/PrazosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attus.prazos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrazosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
